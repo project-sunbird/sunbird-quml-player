5f67444efe836040d6eb627744cc52b14cab9ccb
Vivek Kasture <vivek_kasture@techjoomla.com>
2021-09-15 16:47:44 +0530

    Issue #SB-0000 feat: Send parentId along with question

--- projects/quml-library/src/lib/quml-question-cursor.service.ts
+++ projects/quml-library/src/lib/quml-question-cursor.service.ts
@@ -4,7 +4,7 @@ import { Question } from './quml-library-interface';
 
 export abstract class QuestionCursor {
 
-  abstract getQuestions(identifiers: string[]): Observable<Question>;
+  abstract getQuestions(identifiers: string[], parentId?: string): Observable<Question>;
   abstract getQuestion(identifier: string): Observable<Question>;
   abstract getQuestionSet(identifier: string): Observable<any>;
   abstract getAllQuestionSet(identifiers: string[]): Observable<any>;
