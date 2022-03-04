f8a69b0259afc4daa683cf297de1705bc72be2a3
Vivek Kasture <vivek_kasture@techjoomla.com>
2022-03-04 14:07:50 +0530

    Issue #SB-0000 feat: Added test cases

--- projects/quml-library/src/lib/services/viewer-service/viewer-service.data.ts
+++ projects/quml-library/src/lib/services/viewer-service/viewer-service.data.ts
@@ -1,81 +1,341 @@
 export const mockData = {
-    playerConfig: {
-      context: {
-        mode: 'play',
-        authToken: '',
-        sid: '7283cf2e-d215-9944-b0c5-269489c6fa56',
-        did: '3c0a3724311fe944dec5df559cc4e006',
-        uid: 'anonymous',
-        channel: '505c7c48ac6dc1edc9b08f21db5a571d',
-        pdata: { id: 'prod.diksha.portal', ver: '3.2.12', pid: 'sunbird-portal.contentplayer' },
-        contextRollup: { l1: '505c7c48ac6dc1edc9b08f21db5a571d' },
-        tags: [
-          ''
-        ],
-        cdata: [],
-        timeDiff: 0,
-        objectRollup: {},
-        host: '',
-        endpoint: '',
-        userData: {
-          firstName: 'Diptesh Mukherjee',
-          lastName: 'Gangula'
-        },
-        // if the dispatcher is provided then player won't send the event to backend container need to send it
-        dispatcher: {
-          dispatch: function (event) {
-            console.log(`Events from dispatcher: ${JSON.stringify(event)}`)
-          }
-        }
+  playerConfig: {
+    "context": {
+      "threshold": 3,
+      "mode": "play",
+      "authToken": " ",
+      "sid": "913b3c6c-2874-26dd-ed0c-c23ddc00b718",
+      "did": "561c348e631fd225b46a5571cbd42ad1",
+      "uid": "",
+      "channel": "01268904781886259221",
+      "pdata": {
+        "id": "preprod.diksha.portal",
+        "ver": "3.3.0",
+        "pid": "sunbird-portal.contentplayer"
       },
-      config: {
-        traceId: '123456'
+      "contextRollup": {
+        "l1": "string",
+        "l2": "string",
+        "l3": "string",
+        "l4": "string"
       },
-      // tslint:disable-next-line:max-line-length
-      metadata: { "compatibilityLevel": 4, "copyright": "Kendriya_Vidyalaya,2020", "keywords": ["epub"], "subject": ["Science"], "channel": "diksha", "language": ["English"], "mimeType": "application/epub", "objectType": "Content", "gradeLevel": ["Class 10"], "appIcon": "https://drive.google.com/uc?export=download&id=1z2kHz_wfjcOcDKfenkxWwqIlwtro6uv0", "primaryCategory": "Course Assessment", "artifactUrl": "https://sunbirdstagingpublic.blob.core.windows.net/sunbird-content-staging/content/do_21312960731822489612047/artifact/index.epub", "contentType": "SelfAssess", "identifier": "do_21312960731822489612047", "audience": ["Student"], "visibility": "Default", "mediaType": "content", "osId": "org.ekstep.quiz.app", "languageCode": ["en"], "license": "CC BY 4.0", "name": "EPUB_229.epub", "attributions": ["kanmani"], "status": "Live", "code": "49f3ea6d-db45-a4a7-fcd6-daf58785c7db", "description": "epub", "streamingUrl": "https://sunbirdstagingpublic.blob.core.windows.net/sunbird-content-staging/content/do_21312960731822489612047/artifact/index.epub", "medium": ["English"], "createdOn": "2020-10-15T04:14:28.339+0000", "lastUpdatedOn": "2020-10-15T04:19:30.101+0000", "originData": { "identifier": "do_21312917869280460814945", "repository": "https://dock.preprod.ntp.net.in/api/content/v1/read/do_21312917869280460814945" }, "creator": "classmate2", "pkgVersion": 1, "versionKey": "1602735570101", "framework": "ekstep_ncert_k-12", "createdBy": "7ff59e4d-0f4f-4f25-8244-969423e91a16", "board": "CBSE", "resourceType": "Learn", "orgDetails": {}, "licenseDetails": { "name": "CC BY 4.0", "url": "https://creativecommons.org/licenses/by/4.0/legalcode", "description": "For details see below:" } },
-      data: {}
-    },
-    spineEvent: {
-      items: [
+      "tags": [],
+      "cdata": [
         {
-          'index': 0
+          "id": "c0c9384a82a75f219468d363e1891963",
+          "type": "ContentSession"
+        },
+        {
+          "id": "a12f45a1d7078901adb27b48be4b428d",
+          "type": "PlaySession"
         }
+      ],
+      "timeDiff": 5,
+      "objectRollup": {
+        "l1": "string",
+        "l2": "string",
+        "l3": "string",
+        "l4": "string"
+      },
+      "host": "",
+      "endpoint": "/data/v3/telemetry",
+      "userData": {
+        "firstName": "Diptesh",
+        "lastName": "Mukherjee"
+      }
+    },
+    "metadata": {
+      "lastStatusChangedOn": "2022-02-28T07:56:09.702+0000",
+      "parent": "do_213484313936035840138",
+      "children": [
+        {
+          "copyright": "tn",
+          "lastStatusChangedOn": "2022-02-28T09:22:20.544+0000",
+          "parent": "do_21348431528472576011",
+          "author": "Vivek",
+          "name": "q1",
+          "createdOn": "2022-02-28T07:50:18.088+0000",
+          "channel": "01269878797503692810",
+          "lastUpdatedOn": "2022-02-28T09:22:20.544+0000",
+          "subject": [
+            "English"
+          ],
+          "showTimer": "No",
+          "identifier": "do_21348431559137689613",
+          "gradeLevel": [
+            "Class 4"
+          ],
+          "compatibilityLevel": 4,
+          "audience": [
+            "Teacher"
+          ],
+          "se_boards": [
+            "State (Tamil Nadu)"
+          ],
+          "primaryCategory": "Multiple Choice Question",
+          "se_mediums": [
+            "English"
+          ],
+          "downloadUrl": "https://sunbirdstagingpublic.blob.core.windows.net/sunbird-content-staging/question/do_21348431559137689613/q1_1646040140133_do_21348431559137689613_1.ecar",
+          "se_subjects": [
+            "English"
+          ],
+          "medium": [
+            "English"
+          ],
+          "interactionTypes": [
+            "choice"
+          ],
+          "framework": "tn_k-12_5",
+          "versionKey": "1646034618244",
+          "mimeType": "application/vnd.sunbird.question",
+          "code": "0f3e5084-7126-5094-85d8-08d1187008b3",
+          "license": "CC BY 4.0",
+          "version": 1,
+          "prevStatus": "Draft",
+          "templateId": "mcq-vertical",
+          "language": [
+            "English"
+          ],
+          "board": "State (Tamil Nadu)",
+          "showFeedback": "No",
+          "objectType": "Question",
+          "status": "Live",
+          "createdBy": "fca2925f-1eee-4654-9177-fece3fd6afc9",
+          "contentEncoding": "gzip",
+          "depth": 2,
+          "se_gradeLevels": [
+            "Class 4"
+          ],
+          "se_FWIds": [
+            "tn_k-12_5"
+          ],
+          "allowAnonymousAccess": "Yes",
+          "contentDisposition": "inline",
+          "artifactUrl": "https://sunbirdstagingpublic.blob.core.windows.net/sunbird-content-staging/question/do_21348431559137689613/do_21348431559137689613_1646040138601.zip",
+          "visibility": "Parent",
+          "qType": "MCQ",
+          "showSolutions": "No",
+          "variants": {
+            "full": {
+              "ecarUrl": "https://sunbirdstagingpublic.blob.core.windows.net/sunbird-content-staging/question/do_21348431559137689613/q1_1646040140133_do_21348431559137689613_1.ecar",
+              "size": "2725"
+            },
+            "online": {
+              "ecarUrl": "https://sunbirdstagingpublic.blob.core.windows.net/sunbird-content-staging/question/do_21348431559137689613/q1_1646040140229_do_21348431559137689613_1_ONLINE.ecar",
+              "size": "1312"
+            }
+          },
+          "index": 1,
+          "pkgVersion": 1
+        },
+        {
+          "copyright": "tn",
+          "lastStatusChangedOn": "2022-02-28T09:22:22.524+0000",
+          "parent": "do_21348431528472576011",
+          "author": "Vivek",
+          "name": "q2",
+          "createdOn": "2022-02-28T07:51:56.918+0000",
+          "channel": "01269878797503692810",
+          "lastUpdatedOn": "2022-02-28T09:22:22.524+0000",
+          "subject": [
+            "English"
+          ],
+          "showTimer": "No",
+          "identifier": "do_21348431640099225615",
+          "gradeLevel": [
+            "Class 4"
+          ],
+          "compatibilityLevel": 4,
+          "audience": [
+            "Teacher"
+          ],
+          "se_boards": [
+            "State (Tamil Nadu)"
+          ],
+          "primaryCategory": "Multiple Choice Question",
+          "se_mediums": [
+            "English"
+          ],
+          "downloadUrl": "https://sunbirdstagingpublic.blob.core.windows.net/sunbird-content-staging/question/do_21348431640099225615/q2_1646040142399_do_21348431640099225615_1.ecar",
+          "se_subjects": [
+            "English"
+          ],
+          "medium": [
+            "English"
+          ],
+          "interactionTypes": [
+            "choice"
+          ],
+          "framework": "tn_k-12_5",
+          "versionKey": "1646034716932",
+          "mimeType": "application/vnd.sunbird.question",
+          "code": "f588f3ef-cebe-ca9e-c75e-29b96c1d4223",
+          "license": "CC BY 4.0",
+          "version": 1,
+          "prevStatus": "Draft",
+          "templateId": "mcq-vertical",
+          "language": [
+            "English"
+          ],
+          "board": "State (Tamil Nadu)",
+          "showFeedback": "No",
+          "objectType": "Question",
+          "status": "Live",
+          "createdBy": "fca2925f-1eee-4654-9177-fece3fd6afc9",
+          "contentEncoding": "gzip",
+          "depth": 2,
+          "se_gradeLevels": [
+            "Class 4"
+          ],
+          "se_FWIds": [
+            "tn_k-12_5"
+          ],
+          "allowAnonymousAccess": "Yes",
+          "contentDisposition": "inline",
+          "artifactUrl": "https://sunbirdstagingpublic.blob.core.windows.net/sunbird-content-staging/question/do_21348431640099225615/do_21348431640099225615_1646040142347.zip",
+          "visibility": "Parent",
+          "qType": "MCQ",
+          "showSolutions": "No",
+          "variants": {
+            "full": {
+              "ecarUrl": "https://sunbirdstagingpublic.blob.core.windows.net/sunbird-content-staging/question/do_21348431640099225615/q2_1646040142399_do_21348431640099225615_1.ecar",
+              "size": "2841"
+            },
+            "online": {
+              "ecarUrl": "https://sunbirdstagingpublic.blob.core.windows.net/sunbird-content-staging/question/do_21348431640099225615/q2_1646040142454_do_21348431640099225615_1_ONLINE.ecar",
+              "size": "1368"
+            }
+          },
+          "index": 2,
+          "pkgVersion": 1
+        }
+      ],
+      "name": "Section 1",
+      "navigationMode": "non-linear",
+      "createdOn": "2022-02-28T07:49:40.662+0000",
+      "channel": "01269878797503692810",
+      "generateDIALCodes": "No",
+      "lastUpdatedOn": "2022-02-28T07:56:09.702+0000",
+      "showTimer": "No",
+      "identifier": "do_21348431528472576011",
+      "description": "This is a section 1",
+      "containsUserData": "No",
+      "allowSkip": "Yes",
+      "compatibilityLevel": 5,
+      "primaryCategory": "Practice Question Set",
+      "setType": "materialised",
+      "languageCode": [
+        "en"
+      ],
+      "attributions": [],
+      "versionKey": "1646034580662",
+      "mimeType": "application/vnd.sunbird.questionset",
+      "code": "9ebcf8c7-7a56-4cbc-6b95-240ac386f280",
+      "license": "CC BY 4.0",
+      "version": 1,
+      "prevStatus": "Draft",
+      "showHints": "No",
+      "language": [
+        "English"
+      ],
+      "showFeedback": "Yes",
+      "objectType": "QuestionSet",
+      "status": "Live",
+      "requiresSubmit": "No",
+      "shuffle": true,
+      "contentEncoding": "gzip",
+      "depth": 1,
+      "allowAnonymousAccess": "Yes",
+      "contentDisposition": "inline",
+      "allowBranching": "No",
+      "visibility": "Parent",
+      "showSolutions": "Yes",
+      "index": 1,
+      "timeLimits": {
+        "maxTime": "7200",
+        "warningTime": "900"
+      },
+      "childNodes": [
+        "do_21348431559137689613",
+        "do_21348431640099225615"
       ]
     },
-    endEvent :{
-        type: "END",
-        data: {
-          index: 26,
-          href: "OEBPS/chapter-020-the-end-of-the-middle-ages.html",
-          cfi: "epubcfi(/6/54[chapter-020]!/4/2[the-end-of-the-middle-ages]/2/2/1:0)",
-          displayed: {
-            page: 1,
-            total: 0
-          },
-          location: -1,
-          percentage: 0
-        }
-    },
-    heartBeatEvent: {
-        type: "pageChange",
-        data: {
-          index: 1,
-          href: "OEBPS/title-page.html",
-          cfi: "epubcfi(/6/4[title-page]!/4/2[title-page]/2/1:0)",
-          displayed: {
-            page: 1,
-            total: 0
-          },
-          location: -1,
-          percentage: 0
-        }
-    },
-    assessEvent : {
-      item: 'questionData',
-      index: 'index',
-      pass: 'pass', 
-      score: 'score', 
-      resvalues: 'resValues', 
-      duration: 'duration' 
-}
+    "config": {
+      "traceId": "1234",
+      "sideMenu": {
+        "enable": true,
+        "showShare": true,
+        "showDownload": false,
+        "showReplay": false,
+        "showExit": false
+      },
+      "nextContent": {
+        "name": "Roti aur Kutta",
+        "identifier": "do_231234332232"
+      }
+    }
+  },
+  parentConfig: {
+    "loadScoreBoard": false,
+    "requiresSubmit": true,
+    "isFirstSection": false,
+    "isSectionsAvailable": true,
+    "isReplayed": false,
+    "identifier": "do_213484313936035840138",
+    "contentName": " Vivek Test questionset",
+    "baseUrl": "",
+    "instructions": "<ol><li>Please circle the correct answer</li><li>Do not use mobile phones</li></ol>",
+    "questionCount": 4,
+    "sideMenuConfig": {
+      "enable": true,
+      "showShare": true,
+      "showDownload": false,
+      "showExit": false,
+      "showReplay": false
+    }
+  },
+  spineEvent: {
+    items: [
+      {
+        'index': 0
+      }
+    ]
+  },
+  endEvent: {
+    type: "END",
+    data: {
+      index: 26,
+      href: "OEBPS/chapter-020-the-end-of-the-middle-ages.html",
+      cfi: "epubcfi(/6/54[chapter-020]!/4/2[the-end-of-the-middle-ages]/2/2/1:0)",
+      displayed: {
+        page: 1,
+        total: 0
+      },
+      location: -1,
+      percentage: 0
+    }
+  },
+  heartBeatEvent: {
+    type: "pageChange",
+    data: {
+      index: 1,
+      href: "OEBPS/title-page.html",
+      cfi: "epubcfi(/6/4[title-page]!/4/2[title-page]/2/1:0)",
+      displayed: {
+        page: 1,
+        total: 0
+      },
+      location: -1,
+      percentage: 0
+    }
+  },
+  assessEvent: {
+    item: 'questionData',
+    index: 'index',
+    pass: 'pass',
+    score: 'score',
+    resvalues: 'resValues',
+    duration: 'duration'
   }
+}
--- projects/quml-library/src/lib/services/viewer-service/viewer-service.spec.ts
+++ projects/quml-library/src/lib/services/viewer-service/viewer-service.spec.ts
@@ -3,53 +3,48 @@ import { ViewerService } from './viewer-service';
 import { mockData } from './viewer-service.data';
 import { QumlLibraryService } from '../../quml-library.service';
 import { UtilService } from '../../util-service';
-import {TelemetryType} from '../../telemetry-constants';
- 
-describe('ViewerService', () => {
-    beforeEach(() => {
-      TestBed.configureTestingModule({
-        providers: [UtilService, QumlLibraryService]
-      });
+import { QuestionCursor } from '../../quml-question-cursor.service';
+
+fdescribe('ViewerService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [UtilService, QumlLibraryService, QuestionCursor],
     });
+  });
 
-    it('should be created', () => {
-        const service: ViewerService = TestBed.get(ViewerService);
-        expect(service).toBeTruthy();
-      });
+  it('should be created', () => {
+    const service: ViewerService = TestBed.get(ViewerService);
+    expect(service).toBeTruthy();
+  });
 
-      it('should initialize player config', () => {
-        const service = TestBed.get(ViewerService);
-        service.initialize(mockData.playerConfig);
-        expect(service.src).toEqual(mockData.playerConfig.metadata.artifactUrl);
-        expect(service.endPageSeen).toBeFalsy();
-      });
+  it('should initialize player config', () => {
+    const service = TestBed.get(ViewerService);
+    const questionIds = [
+      "do_21348431559137689613",
+      "do_21348431640099225615"
+    ];
+    const qumlService = TestBed.get(QumlLibraryService);
+    spyOn(qumlService, 'initializeTelemetry');
+    service.initialize(mockData.playerConfig, 3, questionIds, mockData.parentConfig);
+    // expect(service.src).toEqual(mockData.playerConfig.metadata.artifactUrl);
+    // expect(service.endPageSeen).toBeFalsy();
+  });
 
-      it('should raise Start event ', () => {
-        const service = TestBed.get(ViewerService);
-        const qumlLibraryService = TestBed.get(QumlLibraryService);
-        spyOn(service.qumlPlayerEvent, 'emit');
-        spyOn(qumlLibraryService, 'start');
-        service.raiseStartEvent(1);
-        expect(service.qumlPlayerEvent.emit).toHaveBeenCalled();
-        expect(qumlLibraryService.start).toHaveBeenCalled();
-      });
+  it('should raise Start event ', () => {
+    const service = TestBed.get(ViewerService);
+    const qumlLibraryService = TestBed.get(QumlLibraryService);
+    spyOn(service.qumlPlayerEvent, 'emit');
+    spyOn(qumlLibraryService, 'start');
+    service.raiseStartEvent(1);
+    expect(service.qumlPlayerEvent.emit).toHaveBeenCalled();
+    expect(qumlLibraryService.start).toHaveBeenCalled();
+  });
 
-      it('should raise interact event' , () => {
-        const service = TestBed.get(ViewerService);
-        const qumlLibraryService = TestBed.get(QumlLibraryService);
-        spyOn(qumlLibraryService , 'interact');
-        service.raiseHeartBeatEvent('type', 'Interact',  1);
-        expect(qumlLibraryService.interact).toHaveBeenCalled();
-      })
-
-
-      it('should raise error event', () => {
-        const service: ViewerService = TestBed.get(ViewerService);
-        const qumlLibraryService = TestBed.get(QumlLibraryService);
-        spyOn(service.qumlPlayerEvent , 'emit');
-        spyOn(service.qumlLibraryService, 'error');
-        service.raiseErrorEvent(new Error());
-        expect(service.qumlPlayerEvent.emit).toHaveBeenCalled();
-        expect(service.qumlLibraryService.error).toHaveBeenCalled();
-      })
+  it('should raise interact event', () => {
+    const service = TestBed.get(ViewerService);
+    const qumlLibraryService = TestBed.get(QumlLibraryService);
+    spyOn(qumlLibraryService, 'interact');
+    service.raiseHeartBeatEvent('type', 'interact', 1);
+    expect(qumlLibraryService.interact).toHaveBeenCalled();
+  })
 });
