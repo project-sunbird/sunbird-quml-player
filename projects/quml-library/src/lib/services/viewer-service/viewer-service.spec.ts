7132c4bbbc69e88f3b0cb4205d9ed87201c9b5d4
Vivek Kasture <vivek_kasture@techjoomla.com>
2022-03-07 22:54:37 +0530

    Issue #SB-0000 feat: Added test cases

--- projects/quml-library/src/lib/services/viewer-service/viewer-service.spec.ts
+++ projects/quml-library/src/lib/services/viewer-service/viewer-service.spec.ts
@@ -1,14 +1,14 @@
-import { TestBed } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
 import { ViewerService } from './viewer-service';
 import { mockData } from './viewer-service.data';
 import { QumlLibraryService } from '../../quml-library.service';
 import { UtilService } from '../../util-service';
 import { QuestionCursor } from '../../quml-question-cursor.service';
 
-fdescribe('ViewerService', () => {
+describe('ViewerService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [UtilService, QumlLibraryService, QuestionCursor],
+      providers: [ViewerService, UtilService, QumlLibraryService, QuestionCursor],
     });
   });
 
@@ -46,5 +46,106 @@ fdescribe('ViewerService', () => {
     spyOn(qumlLibraryService, 'interact');
     service.raiseHeartBeatEvent('type', 'interact', 1);
     expect(qumlLibraryService.interact).toHaveBeenCalled();
-  })
+  });
+
+  it('should call getSectionQuestions', () => {
+    const viewerService = TestBed.get(ViewerService);
+    viewerService.sectionQuestions = mockData.mockSectionQuestions;
+    const res = viewerService.getSectionQuestions('do_21348431528472576011');
+    console.log(res);
+    expect(res).toBeDefined();
+  });
+
+  it('should call updateSectionQuestions', () => {
+    const viewerService = TestBed.get(ViewerService);
+    viewerService.sectionQuestions = mockData.mockSectionQuestions;
+    viewerService.updateSectionQuestions('do_21348431528472576011', mockData.mockSectionQuestions[0].questions);
+    expect(viewerService.sectionQuestions[0].questions).toBeDefined();
+  });
+
+  it('should call updateSectionQuestions', () => {
+    const viewerService = TestBed.get(ViewerService);
+    viewerService.sectionQuestions = mockData.mockSectionQuestions;
+    viewerService.updateSectionQuestions('do_21348431528472576012', mockData.mockSectionQuestions[0].questions);
+    expect(viewerService.sectionQuestions.length).toEqual(2);
+  });
+
+  it('should call generateMaxAttemptEvents', () => {
+    const viewerService = TestBed.get(ViewerService);
+    viewerService.version = 1;
+    const resp = viewerService.generateMaxAttemptEvents(1, false, false);
+    expect(resp).toBeDefined();
+  });
+
+  it('should call raiseExceptionLog', () => {
+    const viewerService = TestBed.get(ViewerService);
+    const qumlLibraryService = TestBed.get(QumlLibraryService);
+    spyOn(viewerService.qumlPlayerEvent, 'emit');
+    spyOn(qumlLibraryService, 'error');
+    const resp = viewerService.raiseExceptionLog('CPV2_INT_CONNECT_01', 'content failed to load , No Internet Available', 'content failed to load , No Internet Available', 'id');
+    expect(viewerService.qumlPlayerEvent.emit).toHaveBeenCalled();
+    expect(qumlLibraryService.error).toHaveBeenCalled();
+  });
+
+  it('should raiseAssesEvent', () => {
+    const viewerService = TestBed.get(ViewerService);
+    const qumlLibraryService = TestBed.get(QumlLibraryService);
+    spyOn(viewerService.qumlPlayerEvent, 'emit');
+    spyOn(qumlLibraryService, 'startAssesEvent');
+    viewerService.raiseAssesEvent(mockData.questionData, 1, "Yes", 1, mockData.resValues, 2);
+    expect(viewerService.qumlPlayerEvent.emit).toHaveBeenCalled();
+    expect(qumlLibraryService.startAssesEvent).toHaveBeenCalled();
+  });
+
+  it('should raiseResponseEvent', () => {
+    const viewerService = TestBed.get(ViewerService);
+    const qumlLibraryService = TestBed.get(QumlLibraryService);
+    const optionSelected = {
+      "label": "<p><span style=\"background-color:rgb(255,255,255);color:rgb(32,33,36);\">Bill Gates</span></p>",
+      "value": 1,
+      "selected": true
+    }
+    spyOn(viewerService.qumlPlayerEvent, 'emit');
+    spyOn(qumlLibraryService, 'response');
+    viewerService.raiseResponseEvent("do_21348431640099225615", "MCQ", optionSelected);
+    expect(viewerService.qumlPlayerEvent.emit).toHaveBeenCalled();
+    expect(qumlLibraryService.response).toHaveBeenCalled();
+  });
+
+  it('should call raiseEndEvent', () => {
+    const viewerService = TestBed.get(ViewerService);
+    const utilService = TestBed.get(UtilService);
+    const qumlLibraryService = TestBed.get(QumlLibraryService);
+    viewerService.metaData = mockData.metaData;
+    viewerService.sectionQuestions = mockData.mockSectionQuestions;
+    viewerService.qumlPlayerStartTime = 1646553243939;
+    viewerService.version = "1.0";
+    viewerService.totalNumberOfQuestions = 4;
+    spyOn(viewerService.qumlPlayerEvent, 'emit');
+    spyOn(utilService, 'getTimeSpentText');
+    spyOn(qumlLibraryService, 'end');
+    viewerService.raiseEndEvent(4, true, 2);
+    expect(viewerService.qumlPlayerEvent.emit).toHaveBeenCalled();
+    expect(utilService.getTimeSpentText).toHaveBeenCalled();
+    expect(qumlLibraryService.end).toHaveBeenCalled();
+  });
+
+  it('should call raiseSummaryEvent', () => {
+    const viewerService = TestBed.get(ViewerService);
+    const qumlLibraryService = TestBed.get(QumlLibraryService);
+    viewerService.qumlPlayerStartTime = 1646580090765;
+    viewerService.totalNumberOfQuestions = 4;
+    viewerService.version = "1.0";
+    spyOn(viewerService.qumlPlayerEvent, 'emit');
+    spyOn(qumlLibraryService, 'summary');
+    viewerService.raiseSummaryEvent(4, true, 3, {
+      "skipped": 0,
+      "correct": 4,
+      "wrong": 0,
+      "partial": 0
+    });
+
+    expect(viewerService.qumlPlayerEvent.emit).toHaveBeenCalled();
+    expect(qumlLibraryService.summary).toHaveBeenCalled();
+  });
 });
