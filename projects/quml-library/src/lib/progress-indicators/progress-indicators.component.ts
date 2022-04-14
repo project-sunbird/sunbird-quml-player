02bdbedcf4d0b933bf0cdd3815831e5138cbef8d
Vivek Kasture <vivek_kasture@techjoomla.com>
2022-04-14 10:40:30 +0530

    Issue #SB-29184 fix: Updated title for not attempted

--- projects/quml-library/src/lib/progress-indicators/progress-indicators.component.ts
+++ projects/quml-library/src/lib/progress-indicators/progress-indicators.component.ts
@@ -31,7 +31,7 @@ export class ProgressIndicatorsComponent implements OnInit {
     },
     {
       iconText: '1',
-      title: 'Unattempted',
+      title: 'Not viewed',
       class: ''
     },
     {
