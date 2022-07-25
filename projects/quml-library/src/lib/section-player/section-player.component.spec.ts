91be1fbb006939734d99f6d18ca7faea257b6951
Deepdarshan <deepdarshan21@gmail.com>
2022-07-25 12:45:16 +0530

    fix: update file to pass unit test cases

--- projects/quml-library/src/lib/section-player/section-player.component.spec.ts
+++ projects/quml-library/src/lib/section-player/section-player.component.spec.ts
@@ -393,6 +393,7 @@ describe('SectionPlayerComponent', () => {
   it('should return current selected object and selected object are same', () => {
     spyOn(component, 'focusOnNextButton');
     component.currentOptionSelected = { value: 1 };
+    component.myCarousel = myCarousel;
     component.getOptionSelected({ value: 1 });
     expect(component.focusOnNextButton).not.toHaveBeenCalled();
   });
